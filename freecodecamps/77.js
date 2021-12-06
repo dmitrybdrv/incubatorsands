a3e9d7c59ed18292d9084f0ee5f120cca02ceb14
dmitrybdrv <incubator-it@protonmail.com>
2021-12-06 22:47:26 +0300

    homework

---
+++ freecodecamps/77.js
@@ -0,0 +1,19 @@
+function switchOfStuff(val) {
+  // Only change code below this line
+switch(val) {
+  case "a":
+  return "apple"
+  break;
+  case "b":
+  return "bird"
+  break;
+  case "c":
+  return "cat"
+  break;
+  default:
+  return "stuff"
+}
+  // Only change code above this line
+}
+
+switchOfStuff("a");
