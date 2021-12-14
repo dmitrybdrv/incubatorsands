76ec8fb67c2cb1cf698ddf1940f83e34175c3729
dmitrybdrv <incubator-it@protonmail.com>
2021-12-14 20:08:26 +0300

    homework

---
+++ freecodecamps/93.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+myMusic.push({"artist": "Joe Bidden",
+    "title": "RockStar",
+    "release_year": 2020,
+    "formats": [
+      "CD",
+      "DVD",
+      "LP"
+    ],
+    "gold": false})
