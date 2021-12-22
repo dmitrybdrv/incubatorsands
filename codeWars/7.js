018b73aa276076de4743abe15511280cacde9873
dmitrybdrv <incubator-it@protonmail.com>
2021-12-22 17:01:35 +0300

    homework

---
+++ codeWars/7.js
@@ -0,0 +1,8 @@
+Vowel remover
+
+function shortcut(string) {
+var newString = ""
+    newString = string.split("e").join('').split("a").join('').split("o").join("").split("u")
+        .join("").split("i").join("")
+        return newString
+    }
